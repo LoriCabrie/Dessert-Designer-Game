bbcc3f8ff3aa124bdb2d3490c45324b1af47c198
LoriCabrie <lcabrie@charlotte.edu>
2024-09-18 12:41:13 -0400

    Update Dessert.java

--- Dessert.java
+++ Dessert.java
@@ -1,1 +1,16 @@
+public abstract class Dessert {
+    private int type;
+
+    public Dessert(int type) {
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
 
